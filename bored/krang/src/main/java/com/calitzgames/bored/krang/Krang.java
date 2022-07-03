1968d27f6f1bc95d8b581234b38e0438ab54835e
Calle <senseicreep@gmail.com>
2022-07-03 10:00:04 +0200

    Add setInformation and displayInformation.

--- bored/krang/src/main/java/com/calitzgames/bored/krang/Krang.java
+++ bored/krang/src/main/java/com/calitzgames/bored/krang/Krang.java
@@ -1,5 +1,5 @@
 package com.calitzgames.bored.krang;
-
+import com.calitzgames.bored.krang.dts.Information;
 import java.lang.*;
 
 public class Krang {
@@ -7,6 +7,15 @@ public class Krang {
 	public String printLin(String input) {
 		System.out.println(input);
 		return input;
+	}
+
+	public Information setInformation(String header, int value) {
+		return new Information(header, value);
+	}
+
+	public void displayInformation(Information informationObj) {
+		System.out.println("Information header : " + informationObj.getHeader());
+		System.out.println("Information value : " + informationObj.getValue());
 	}
 
 }
