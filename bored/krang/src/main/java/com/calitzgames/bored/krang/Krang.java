e0f6765b3425e0f96927a7a2b13355789ff5506a
Calle <senseicreep@gmail.com>
2022-07-03 14:42:54 +0200

    Add method for writing information objects to Json files.

--- bored/krang/src/main/java/com/calitzgames/bored/krang/Krang.java
+++ bored/krang/src/main/java/com/calitzgames/bored/krang/Krang.java
@@ -4,9 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import jdk.nashorn.api.scripting.JSObject;
 
+import java.io.Writer;
 import java.lang.*;
-
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Krang {
 
@@ -24,11 +25,22 @@ public class Krang {
 		System.out.println("Information value : " + informationObj.getValue());
 	}
 
-	public void printInformationObjJson(Information informationObj){
+	public void printInformationObjJson(Information informationObj) {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		System.out.println(gson.toJson(informationObj));
 
 	}
 
+	public void writeToJsonInformation(Information informationObj) {
+		Gson gson = new Gson();
+		try {
+			Writer writer = Files.newBufferedWriter(Paths.get("jsonfiles/information.json"));
+			gson.toJson(informationObj, writer);
+			writer.close();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+	}
 
 }
