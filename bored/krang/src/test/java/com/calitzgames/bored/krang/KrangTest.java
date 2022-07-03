f65277385549920a394a5b322124f9a26029460c
Calle <senseicreep@gmail.com>
2022-07-03 09:59:14 +0200

    Add new test case for datatype methods in krang.

--- bored/krang/src/test/java/com/calitzgames/bored/krang/KrangTest.java
+++ bored/krang/src/test/java/com/calitzgames/bored/krang/KrangTest.java
@@ -1,5 +1,5 @@
 package com.calitzgames.bored.krang;
-
+import com.calitzgames.bored.krang.dts.Information;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,10 +8,20 @@ public class KrangTest {
 	private final Krang mKrang = new Krang();
 
 	@Test
-	public void print_lin_test() {
+	public void printLinTest() {
 		String testStr = "This is a test string";
 		String returnedStr = mKrang.printLin(testStr);
 
 		Assert.assertEquals(testStr, returnedStr);
 	}
+
+	@Test
+	public void setInformationTest(){
+		Information information = new Information("Test obj string value", 100);
+		mKrang.displayInformation(information);
+		Assert.assertEquals(information.getHeader(),"Test obj string value");
+		Assert.assertEquals(information.getValue(), 100);
+
+	}
+
 }
