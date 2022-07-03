24bb4360c1afa4ce4c8557dc238ebea0c4f98dfe
Calle <senseicreep@gmail.com>
2022-07-03 08:25:36 +0200

    Formatting.

--- bored/krang/src/main/java/com/calitzgames/bored/krang/Krang.java
+++ bored/krang/src/main/java/com/calitzgames/bored/krang/Krang.java
@@ -1,12 +1,12 @@
 package com.calitzgames.bored.krang;
+
 import java.lang.*;
 
 public class Krang {
 
-    public String print_lin(String input){
-        System.out.println(input);
-
-        return input;
-    }
+	public String printLin(String input) {
+		System.out.println(input);
+		return input;
+	}
 
 }
--- bored/krang/src/test/java/com/calitzgames/bored/krang/KrangTest.java
+++ bored/krang/src/test/java/com/calitzgames/bored/krang/KrangTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 
 public class KrangTest {
 
-    private final Krang m_krang = new Krang();
-    @Test
-    public void print_lin_test(){
-        String test_str = "This is a test string";
-        String returned_str = m_krang.print_lin(test_str);
+	private final Krang m_krang = new Krang();
 
-        Assert.assertEquals(test_str, returned_str);
-    }
+	@Test
+	public void print_lin_test() {
+		String test_str = "This is a test string";
+		String returned_str = m_krang.printLin(test_str);
+
+		Assert.assertEquals(test_str, returned_str);
+	}
 }
